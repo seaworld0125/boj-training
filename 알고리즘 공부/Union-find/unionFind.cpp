012028b38862dbf04fa99b4da16131837ead6a69
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-03-01 20:44:20 +0900

    study

--- 알고리즘 공부/Union-find/unionFind.cpp
+++ 알고리즘 공부/Union-find/unionFind.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int findParent(int* parent, int now) {
     if(parent[now] == now) return now;
-    return parent[now] = findParent(parent, parent[now]);
+    return now = findParent(parent, parent[now]);
 }
 
 void unionParent(int* parent, int a, int b) {
@@ -33,20 +33,42 @@ int main() {
 
     // 처음 부모 노드 출력
     printParent(parent);
+    
+    /*
+    출력 결과
+    node    1 2 3 4 5 6 7 8 9 10
+    parent  1 2 3 4 5 6 7 8 9 10
+    */
 
     // 1과 2를 연결
     unionParent(parent, 1, 2);
     printParent(parent);
+    
+    /*
+    출력 결과
+    node    1 2 3 4 5 6 7 8 9 10
+    parent  1 1 3 4 5 6 7 8 9 10
+    */
 
     // 3과 4, 4와 5, 5와 6을 연결
     unionParent(parent, 3, 4);
     unionParent(parent, 4, 5);
     unionParent(parent, 5, 6);
     printParent(parent);
+    
+    /*
+    출력 결과
+    node    1 2 3 4 5 6 7 8 9 10
+    parent  1 1 3 3 3 3 7 8 9 10
+    */
 
     // 2와 6을 연결
     unionParent(parent, 2, 6);
     printParent(parent);
+    
+    /*
+    출력 결과
+    node    1 2 3 4 5 6 7 8 9 10
+    parent  1 1 1 3 3 3 7 8 9 10
+    */
 }
-
-
