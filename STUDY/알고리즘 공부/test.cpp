0dc525ee39c1d3c16fb48e23f09e2c06ec69b7f0
Tae-Kyoung Kim <75168305+seaworld0125@users.noreply.github.com>
2022-04-07 23:01:39 +0900

    study

---
+++ STUDY/알고리즘 공부/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bitset>
+#include <queue>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int a, b;
+    for(int i = 0; i < N; i++) {
+        cin >> a >> b; 
+        cout << a << ", " << b << endl;        
+    }
+}
